78cb2f1720a5964e7cb3b3d0ca26c24dc6003e1d
abe <abestbet1@gmail.com>
2017-01-30 14:09:54 -0800

    update user model for forgot password functionality

--- models/user.model.js
+++ models/user.model.js
@@ -7,7 +7,9 @@ var schema = new Schema({
     lastName: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    tickets: [{type: Schema.Types.ObjectId, ref: 'Ticket'}]
+    tickets: [{type: Schema.Types.ObjectId, ref: 'Ticket'}],
+    reset_password_token: String,
+    reset_password_set_at: String
 });
 
 schema.plugin(mongooseUniqueValidator);
