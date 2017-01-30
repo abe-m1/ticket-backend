05b9e1e6dbca3c7185ee44c623af2e34cd536f23
abe <abestbet1@gmail.com>
2017-01-30 14:01:47 -0800

    fix error on getOneUser

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -102,17 +102,16 @@ exports.getAllUsers =function(req, res, next) {
 
 
 exports.getOneUser =  function( req, res, next ) {	
-  	User.findById( req.params.id )
-		.exec( function( err, user ) {
-//  		if (err) {
+  	User.findById( req.params.id , function(err, user){
+          if (err) {
             return res.status(500).json({
                 title: 'An error occurred',
                 error: err
             });
-        })
+        }
         res.status(200).json({
             message: 'Successfully returned all user',
             user: user          
         });
-
+      })
 }
