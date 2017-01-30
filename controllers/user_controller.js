bd5d65cb6eb063a4fa5e1d881f27bee6e85e9c2b
abe <abestbet1@gmail.com>
2017-01-30 13:58:51 -0800

    create controller function to get one user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -99,3 +99,20 @@ exports.getAllUsers =function(req, res, next) {
 
    })
 }
+
+
+exports.getOneUser =  function( req, res, next ) {	
+  	User.findById( req.params.id )
+		.exec( function( err, user ) {
+//  		if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        })
+        res.status(200).json({
+            message: 'Successfully returned all user',
+            user: user          
+        });
+
+}
