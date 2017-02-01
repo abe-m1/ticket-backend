88b6295773cc962e8022116d191597cd295fdbf2
abe <abestbet1@gmail.com>
2017-02-01 12:31:59 -0800

    implement protected routes

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var Message = require('../models/message')
+var jwt = require('jsonwebtoken')
 var TicketController = require('../controllers/ticket_controller')
 
-var User = require('../models/user.model');
 
-router.post('/', TicketController.newTicket);
+
 router.get('/', TicketController.getAllTickets)
+router.use('/', function(req, res, next){
+    jwt.verify(req.query.token, 'secret', function(err, decoded){
+        //get decoded back, difference between verify and decode, below
+        if (err){
+            return res.status(401).json({
+                title: 'not authenticated',
+                error: err
+            })
+        }
+        next()
+    })
+})
+router.post('/', TicketController.newTicket);
+
 router.patch('/:id', TicketController.editTicket)
 router.delete('/id', TicketController.deleteTicket)
 
