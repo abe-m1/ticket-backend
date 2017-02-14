cfc9a60928593f10e14fba8997cd3688ad395bd9
abe <abestbet1@gmail.com>
2017-02-14 09:15:23 -0800

    reconfigure user routes.js

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,33 @@
-var express = require('express');
-var router = express.Router();
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+// var express = require('express');
+// var router = express.Router();
+// var bcrypt = require('bcryptjs');
+// var jwt = require('jsonwebtoken');
+// var UserController = require('../controllers/user_controller')
+
+// var User = require('../models/user.model');
+
+// router.post('/', UserController.signup);
+
+// router.post('/signin', UserController.signin);
+// router.get('/', UserController.getAllUsers)
+// router.get('/:id', UserController.getOneUser)
+// router.patch('/:id', UserController.editUser)
+// router.delete('/:id', UserController.deleteUser)
+
+// module.exports = router;
+
+
+
+
+
 var UserController = require('../controllers/user_controller')
 
-var User = require('../models/user.model');
+function init(Router) {
+     Router.post('/authenticate', userController.signup)
 
-router.post('/', UserController.signup);
+//     Router.route('/forgotpw')
+//         .post(userController.forgotPW)
+//         .put(userController.changePW)
+ }
 
-router.post('/signin', UserController.signin);
-router.get('/', UserController.getAllUsers)
-router.get('/:id', UserController.getOneUser)
-router.patch('/:id', UserController.editUser)
-router.delete('/:id', UserController.deleteUser)
-
-module.exports = router;
+module.exports.init = init
