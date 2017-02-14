2218b7165446a32e3857a9506b394fb9b2b67193
abe <abestbet1@gmail.com>
2017-02-14 10:22:16 -0800

    clean up ticket routes

--- routes/ticket.js
+++ routes/ticket.js
@@ -4,32 +4,6 @@
  var ticketController = require('../controllers/ticket_controller')
 
 
-
-// router.get('/', TicketController.getAllTickets)
-// router.use('/', function(req, res, next){
-//     jwt.verify(req.headers.token, 'secret', function(err, decoded){
-//         //get decoded back, difference between verify and decode, below
-//         if (err){
-//             return res.status(401).json({
-//                 title: 'not authenticated',
-//                 error: err
-//             })
-//         }
-//         next()
-//     })
-// })
-// router.post('/', TicketController.newTicket);
-// router.patch('/:id', TicketController.editTicket)
-// router.delete('/id', TicketController.deleteTicket)
-
-
-
-
-// module.exports = router;
-
-
-
-
 function init(Router) {
     
     Router.get('/ticket', ticketController.getAllTickets)
