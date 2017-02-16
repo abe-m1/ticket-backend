a104e16ef77d67d01fa5f5ad7e1280b3d41760ee
abe <abestbet1@gmail.com>
2017-02-16 12:18:01 -0800

    point user model references to index.js

--- controllers/ticket_controller.js
+++ controllers/ticket_controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.model')
+const User = require('../models/user')
 const Ticket = require('../models/ticket.model')
 var bcrypt = require('bcryptjs');
 const jwt  = require('jsonwebtoken')
