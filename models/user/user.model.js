c45686fdcf3323de03da153d0e8865b118f1e9a2
abe <abestbet1@gmail.com>
2017-02-16 12:28:02 -0800

    incorporate seperate userMethods.js into user

--- models/user/user.model.js
+++ models/user/user.model.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 const userModel = require('./userModel.js')
 var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
+const userMethods = require('./userMethods.js')
 
 userSchema = new mongoose.Schema(userModel)
 
 userSchema.plugin(mongooseUniqueValidator);
+
+userMethods(userSchema)
 
 module.exports = mongoose.model('User', userSchema);
 
