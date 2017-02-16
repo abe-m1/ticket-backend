bf32981e76c8d3a757f6109e6b30f2bef4b56195
abe <abestbet1@gmail.com>
2017-02-16 12:59:15 -0800

    all ticketModel files passed through index.js

--- controllers/ticket_controller.js
+++ controllers/ticket_controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-const Ticket = require('../models/ticket.model')
+const Ticket = require('../models/ticket')
 var bcrypt = require('bcryptjs');
 const jwt  = require('jsonwebtoken')
 
