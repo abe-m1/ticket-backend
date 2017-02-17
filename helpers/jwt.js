58e2476335587bebe0fb617d6be4381feb93936c
abe <abestbet1@gmail.com>
2017-02-17 11:35:19 -0800

    helper to decode incoming tokens

---
+++ helpers/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const secret = require('../config').jwtSecret
+
+function verifyJwt(token){
+    return jwt.verify(token, secret)
+
+}
+
+function signJwt(data) {
+    return jwt.sign(data, secret)
+}
+
+
+function auth(req, res, next){
+    const token = req.headers['authorization']
+
+    if (!token) throw { message: 'missing token', status: 401 }
+
+    const decoded = verifyJwt(token)
+
+    if (!decoded) throw { message: 'bad token', status: 401 }
+
+    req.decoded = decoded
+
+    next()
+}
