171ddb9c47d700359d2b6c1fa6846fff0af8a9cb
abe <abestbet1@gmail.com>
2017-02-17 11:36:48 -0800

    store json secret in dev.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const secret = require('../config').jwtSecret
+const secret = require('../config/dev.js').jwtSecret
 
 function verifyJwt(token){
     return jwt.verify(token, secret)
