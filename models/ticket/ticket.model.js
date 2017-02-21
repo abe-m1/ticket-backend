acc842f361f20a1589ce51b85ab5a5457cc7bbfc
abe <abestbet1@gmail.com>
2017-02-21 10:12:52 -0800

    restructure ticket model

--- models/ticket/ticket.model.js
+++ models/ticket/ticket.model.js
@@ -1,13 +1,8 @@
 const mongoose  = require('mongoose')
 const Schema = mongoose.Schema
 const ticketModel = require('./ticketModel.js')
-const ticketMethods = require('./ticketMethods.js')
-const ticketStatics = require('./ticketStatics.js')
 
 ticketSchema = new mongoose.Schema(ticketModel)
-
-ticketMethods(ticketSchema)
-ticketStatics(ticketSchema)
 
 const ModelClass = mongoose.model('ticket', ticketSchema)
 module.exports = ModelClass
