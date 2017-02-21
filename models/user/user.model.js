c97bb1e6a38cd4da0452733c5a716b57b795fbae
abe <abestbet1@gmail.com>
2017-02-21 10:11:29 -0800

    restructure models remove statics and methods

--- models/user/user.model.js
+++ models/user/user.model.js
@@ -2,17 +2,10 @@ var mongoose = require('mongoose');
 const userModel = require('./userModel.js')
 var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
-const userMethods = require('./userMethods.js')
-const userStatics = require('./userStatics.js')
 
 userSchema = new mongoose.Schema(userModel)
 
 userSchema.plugin(mongooseUniqueValidator);
-
-userMethods(userSchema)
-userStatics(userSchema)
-
-
 
 module.exports = mongoose.model('User', userSchema);
 
