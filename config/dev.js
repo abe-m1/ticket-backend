d55a47051dbb56c5dde72272582c90ed450ef496
abe <abestbet1@gmail.com>
2017-02-22 13:45:20 -0800

    configured smtp in dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,13 @@
 module.exports = { db: 'localhost:27017/ticket3',
-                     jwtSecret: 'secret'
+                     jwtSecret: 'secret',
+                     smtp: {
+                        service: 'Gmail',
+                        auth: {
+                            user: 'abedeveloper@gmail.com',
+                            pass: 'painfree1'
+                        },
+                        logger: true, // log to console
+                        debug: true // include SMTP traffic in the logs
+                        }
+                    }
                     }
