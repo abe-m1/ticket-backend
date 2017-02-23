53dc22a26702369e4afe45b55f832a44c628849d
abe <abestbet1@gmail.com>
2017-02-23 11:35:13 -0800

    pulled email messages out of functions

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -245,14 +245,8 @@ function sendEmail(email, token) {
 
 }
 
-function sendEmailOnReset(email, token) {
-    const mailOptions = {
- 				to: user.email,
- 				from: 'TicketApp Password Reset',
- 				subject: 'Your password has been changed',
- 				text: 'Hello,\n\n' +
- 				'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
- 			}
+function sendEmailOnReset(email, mailOptions, token ) {
+    //mailOptions
 
     Helpers.email.transporter.sendMail(mailOptions, (err, info) => {
         if (err) console.log(err)
@@ -263,6 +257,24 @@ function sendEmailOnReset(email, token) {
     })
 
 }
+
+const mailOptionsForForgot = {
+        from: 'ticketApp',
+        to: email,
+        subject: 'TicketApp Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://localhost:8100#/app' + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+    }
+
+const mailOptionsForReset = {
+ 				to: user.email,
+ 				from: 'TicketApp Password Reset',
+ 				subject: 'Your password has been changed',
+ 				text: 'Hello,\n\n' +
+ 				'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+ 			}
 
 
 
